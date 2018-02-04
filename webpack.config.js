2dd01f9fb67940efd7194c8fa0da2c5a7347ed07
Aonghus Flynn <developer@aonghusflynn.com>
2018-02-04 12:06:24 +0000

    changed scripts to app in order to set up the dev server

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build'),
+};
+
+const commonConfig = {
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js',
+  },
+  plugins: [
+    new  HtmlWebpackPlugin({
+      title: 'Webpack Demo'
+    }),
+  ],
+};
+
+const productionConfig = () => commonConfig;
+
+const developmetnConfig = () => {
+  const config = {
+    devServer: {
+
+      stats: 'errors-only',
+
+      host: process.env.HOST,
+      port: process.env.PORT,
+    },
+  };
+
+  return Object.assign(
+      {},
+      commonConfig,
+      config
+    );
+};
+
+module.exports = (env) => {
+  return commonConfig;
+  if(env === "production"){
+    return productionConfig();
+  }
+  return developmetnConfig();
+};
